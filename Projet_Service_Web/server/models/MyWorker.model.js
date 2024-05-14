c5bb843e54a8737f79e72f395de76836465ddbba
Geoffrey Anquetil <geoffrey.anquetil1@gmail.com>
2024-05-14 15:02:55 +0200

    added tp

---
+++ Projet_Service_Web/server/models/MyWorker.model.js
@@ -0,0 +1,106 @@
+
+const { Worker, workerData } = require('worker_threads')
+
+const workerScripts = [];
+workerScripts['worker3'] = './server/models/workerScripts/worker3.js';
+
+const statusSet = new Set(['installed','activated','idle','terminated']);
+
+const fs = require('fs');
+
+
+class MyWorker{
+    constructor({workerName,scriptName,workersService}){
+        this.workerName = workerName;
+        this.scriptFile = workerScripts[scriptName];
+        this.workersService = workersService
+        this.job;
+        this.status = 'installed';
+        this.workersService.set(this.workerName,this);
+        this.logFile = "./logs/" + workerName + ".log";
+    }
+
+    start(){
+        const worker = new Worker( this.scriptFile, {workerData: {workerName:this.workerName}} );
+        this.job = worker;
+
+        worker.on(
+            'online', 
+            () => { 
+                this.status = 'activated';
+                console.log('Launching intensive CPU task') 
+            }
+        ); 
+        worker.on(
+            'message', 
+            messageFromWorker => {
+                console.log(`message from worker  ${this.workerName}: ${messageFromWorker}`)
+            }
+        );
+        worker.on(
+            'error', 
+            (code)=>{ throw Error(`Worker ${this.workerName} issued an error with code ${code}`)}
+        );
+        worker.on(
+            'exit', 
+            code => {
+                this.status = 'terminated';
+            }
+        );
+          
+    }
+
+    dump(){
+        return `This is worker ${this.workerName}`
+    }
+
+    kill(){
+        if(this.status=='activated' || 'idle'==this.status=='activated'){
+            this.job.terminate()
+        }
+    }
+
+    delete(){
+        this.kill();
+        this.workersService.delete(this.workerName);
+    }
+
+    suspend(){
+        this.job.postMessage('suspend')
+        this.status='idle';
+    }
+
+    continue(){
+        this.job.postMessage('continue')
+    }
+
+    isStatus(status){
+        return (status == this.status);
+    }
+
+    setStatus(status){
+        console.log(`setStatus to ${status}, current is ${this.status}`)
+        if('terminated'== status){
+            this.delete();
+        }
+        if('idle' == status && this.status == 'activated' ){
+            this.suspend();
+        }
+        if('activated' == status){
+            if( this.status == 'idle' ){
+                this.continue();
+            }
+            if(  this.status == 'installed'){
+                this.start();
+            }
+        }
+    }
+
+    getStatus(){
+        return this.status;
+    }
+}
+
+
+module.exports = MyWorker;
+  
